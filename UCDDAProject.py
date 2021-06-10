b9872927f6236236dd46309b5f19d9643d6465c2
DavidWhyte97 <81186311+DavidWhyte97@users.noreply.github.com>
2021-06-11 00:29:16 +0100

    Updated graphs
    New graphs

--- UCDDAProject.py
+++ UCDDAProject.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -26,9 +27,9 @@ redwine.loc[0,:]
 rwsulph= redwine["sulphates"].sort_index()
 rwsulph.iloc[:100]
 
-# use itterrows
-for alcohol, row in redwine.iterrows():
-    print(alcohol, row[0], row[1], row[2], row[3],)
+# # use itterrows
+# for alcohol, row in redwine.iterrows():
+#     print(alcohol, row[0], row[1], row[2], row[3],)
 
 # creating new dictionary and converting it to a dataframe
 newdict = {"alcohol": [9 , 7, 9.7], "pH": [3, 3.6, 4]}
@@ -48,19 +49,23 @@ newdf.isnull().sum()
 import numpy as np
 redwine_np = np.array(redwine)
 
-#getting max and min alcohol levels
-print(np.max(redwine["alcohol"]))
-print(np.min(redwine["alcohol"]))
-print(np.mean(redwine["alcohol"]))
-print(np.median(redwine["alcohol"]))
+def print_stat_for_alcohol(stat):
+    np_stat = getattr(np, stat)(redwine["alcohol"])
+    print("Stat {} for alcohol content is: {}".format(stat, np_stat))
+    return;
 
-#using MatPlotLib to visualise Data
-# using a simple linegraph to plot any correlation between pH and alcohol
+# Print KPI's for Alcohol content
+print_stat_for_alcohol("max")
+print_stat_for_alcohol("min")
+print_stat_for_alcohol("mean")
+print_stat_for_alcohol("median")
+
+# #using MatPlotLib to visualise Data
+# # using a simple linegraph to plot any correlation between pH and alcohol
 x=(list(redwine["alcohol"]))
 y=(list(redwine["pH"]))
 
 plt.plot(x, y)
-plt.show()
 
 #using a scatter plot instead with labelled axis
 plt.xlabel("Alcohol")
@@ -68,25 +73,41 @@ plt.ylabel("pH")
 plt.scatter(x, y)
 plt.show()
 
+# Second Visual using Seaborn
+datafra = pd.DataFrame(redwine.loc[0:100])
+fig, ax = plt.subplots()
+sns.countplot(x='alcohol', data=datafra).set_title('Count by Alcohol Content')
+ax.set(xlabel="Alcohol Content", ylabel='Count')
+plt.savefig('CountByAlcoholContent.jpg')
+plt.show()
+# High percentage within subset are withing 9.2 - 9.6 range in terms of alcohol content
+
+# Third Visual
+redwine = pd.read_csv("winequality-red.csv")
+datafra = pd.DataFrame(redwine)
+fig, ax = plt.subplots()
+sns.countplot(x='quality', data=datafra).set_title('Count by Quality')
+ax.set(xlabel="Quality", ylabel='Count')
+plt.savefig('ByQuality.jpg')
+plt.show()
+# The majority of market is in middle - upper middle quality Wine. Does this also determine cost?
+# If I was making a wine the best quality is not of the upmost importance, but it cannot be awful
 
 
+# third Visual
+fig, ax = plt.subplots()
+sctplot = sns.scatterplot(y='density', x='alcohol', data=redwine, marker='o')
+ax.set(xlabel="Alcohol Content", ylabel="Density of Wine")
+sctplot.set_title('Density vs Alcohol')
+plt.show()
+plt.savefig('DensityVsAlcohol.jpg')
+# As Density Decreases, in general so too does alcohol content
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# fourth Visual
+fig, ax = plt.subplots()
+sctplot = sns.scatterplot(y='pH', x='fixed acidity', data=redwine, marker='o')
+ax.set(xlabel="Acidity", ylabel = "pH level")
+sctplot.set_title('pH Level vs Fixed Acidity')
+plt.show()
+plt.savefig('AcidVsPh.jpg')
 
